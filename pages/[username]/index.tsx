684b94a02dff203736da17ae18951c7cde843c8b
Rodrigo Bestard Pino <rbestardpino@est.frba.utn.edu.ar>
2022-02-21 10:48:45 -0300

    static props and paths for /[username]

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -2,41 +2,69 @@ import Metatags from "@components/Metatags";
 import { UserContext } from "@lib/context";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
+import { GetStaticPaths, GetStaticProps } from "next";
+import { ParsedUrlQuery } from "querystring";
 import { useContext } from "react";
+import { db } from "@lib/services/firebase";
+import { query, collection, getDocs, doc, getDoc } from "firebase/firestore";
+import {
+  Review,
+  reviewFromFirestore,
+  User,
+  userFromFirestore,
+} from "@lib/models";
 
-// export async function getServerSideProps({ query: urlQuery }) {
-//   const { username } = urlQuery;
+interface Params extends ParsedUrlQuery {
+  username: string;
+}
 
-//   const userDoc = await getUserWithUsername(username);
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { username } = context.params as Params;
 
-//   // If no user, short circuit to 404 page
-//   if (!userDoc) {
-//     return {
-//       notFound: true,
-//     };
-//   }
+  const user = userFromFirestore(await getDoc(doc(db, `users/${username}`)));
 
-//   // JSON serializable data
-//   let user = null;
-//   let reviews = null;
+  if (!user)
+    return {
+      notFound: true,
+    };
 
-//   if (userDoc) {
-//     user = userDoc.data();
-//     reviews = await getNReviewsOf(userDoc.id, 5);
-//     if (reviews) {
-//       for (const rev of reviews) {
-//         rev.movie = await getMovie(rev.movie, {});
-//       }
-//     }
-//   }
+  const reviews = (
+    await getDocs(query(collection(db, `users/${username}/reviews`)))
+  ).docs.map(reviewFromFirestore);
 
-//   return {
-//     props: { user, reviews: reviews }, // will be passed to the page component as props
-//   };
-// }
+  return {
+    props: {
+      user,
+      reviews,
+    },
+    revalidate: 3600,
+  };
+};
 
-export default function UserProfile() {
-  const { user, reviews } = useContext(UserContext);
+export const getStaticPaths: GetStaticPaths = async () => {
+  const usernamesSnap = await getDocs(collection(db, "usernames"));
+
+  const paths: { params: { username: string } }[] = [];
+  usernamesSnap.forEach((usernameDoc) => {
+    paths.push({
+      params: {
+        username: usernameDoc.id,
+      },
+    });
+  });
+
+  return {
+    paths: paths,
+    fallback: "blocking",
+  };
+};
+
+interface Props {
+  user: User;
+  reviews: Review[];
+}
+
+export default function UserProfile({ user, reviews }: Props) {
   return (
     <main>
       <Metatags
