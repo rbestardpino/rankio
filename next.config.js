d0bdbc4d896b32fc3f5339859f1a853ccf147959
Rodrigo Bestard Pino <rbestardpino@est.frba.utn.edu.ar>
2022-03-24 20:54:29 -0300

    made it a pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+
+module.exports = withPWA({
   reactStrictMode: true,
   images: {
     domains: ["image.tmdb.org"],
   },
-};
+  pwa: {
+    dest: "public",
+    runtimeCaching,
+  },
+});
